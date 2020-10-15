8dfd50cd6ef0609e4498b6a3959a384fa174f8d5
Igor <igorko.by@gmail.com>
2020-10-15 12:32:10 +0300

    init: start project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles/index.scss'
+
+// import App from './components/App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    Hello world
+    {/* <App /> */}
+  </React.StrictMode>,
+  document.getElementById('root'),
+)
